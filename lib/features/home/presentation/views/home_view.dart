c72b9ba5f383ed8d834f6d6c5056421d50651eee
kerolosragaie <kerolosragaie@gmail.com>
2023-05-31 20:27:09 +0200

    creating HomeViewBody

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webviewer/features/home/presentation/views/widgets/app_bar_widget.dart';
+import 'package:webviewer/features/home/presentation/views/widgets/body_home_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -8,6 +9,7 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: AppbarWidget(),
+      body: HomeViewBody(),
     );
   }
 }
