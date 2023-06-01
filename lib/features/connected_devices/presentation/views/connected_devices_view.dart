3a77af6ad447e9bd888cd532315f3e2be561056b
kerolosragaie <kerolosragaie@gmail.com>
2023-06-01 20:45:22 +0200

    Adding mobx, provider and connceted devices body

--- lib/features/connected_devices/presentation/views/connected_devices_view.dart
+++ lib/features/connected_devices/presentation/views/connected_devices_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webviewer/features/connected_devices/presentation/views/widgets/app_bar_widget.dart';
+import 'package:webviewer/features/connected_devices/presentation/views/widgets/body_connected_devices.dart';
 
 class ConnectedDevicesView extends StatelessWidget {
   const ConnectedDevicesView({super.key});
@@ -8,9 +9,7 @@ class ConnectedDevicesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: AppbarWidget(),
-      body: Column(
-        children: [],
-      ),
+      body: ConnectedDevicesBody(),
     );
   }
 }
