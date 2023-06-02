f3c476e498ffc27d886f546798969f50184c28d6
kerolosragaie <kerolosragaie@gmail.com>
2023-06-02 14:00:33 +0200

    removing unused Styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const textStyle18 = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.w600,
-  );
   static const textStyle14 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.normal,
@@ -13,16 +9,12 @@ abstract class Styles {
     fontSize: 16,
     fontWeight: FontWeight.w500,
   );
-  static const textStyle30 = TextStyle(
-    fontSize: 30,
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
     fontWeight: FontWeight.normal,
   );
   static const textStyle26 = TextStyle(
     fontSize: 26,
-    fontWeight: FontWeight.normal,
-  );
-  static const textStyle20 = TextStyle(
-    fontSize: 20,
     fontWeight: FontWeight.normal,
   );
 }
